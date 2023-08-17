746ce0cfd71503302103617dd8184ce078fbea32
Tzyber <134324777+Tzyber@users.noreply.github.com>
2023-08-17 20:56:37 +0200

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import App from "@/App.vue";
 import summary from "@/components/summary.vue";
 
 const router = createRouter({
@@ -15,8 +14,18 @@ const router = createRouter({
       path: '/aboutme',
       name: 'aboutme',
       component: summary
-    },
+    }
   ]
 })
+
+// Hinzugefügter Navigation Guard
+router.beforeEach((to, from, next) => {
+  if (to.matched.length === 0) {
+    // Wenn keine Route übereinstimmt, leite auf die HomeView um
+    next({ name: 'home' });
+  } else {
+    next(); // Erlaube die Navigation
+  }
+});
 
 export default router
